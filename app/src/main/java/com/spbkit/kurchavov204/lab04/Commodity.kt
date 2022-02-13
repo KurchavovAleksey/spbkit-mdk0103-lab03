e55123d12ade90b241c2e7a3f14d5553a3e23e58
Aleksey Kurchavov <kurchavov.aleksey@mail.ru>
2022-02-14 01:35:18 +0300

    Add authorship comments to code

--- app/src/main/java/com/spbkit/kurchavov204/lab04/Commodity.kt
+++ app/src/main/java/com/spbkit/kurchavov204/lab04/Commodity.kt
@@ -9,97 +9,97 @@ import android.widget.Toast
 
 
 class CommoditiesUI() {
-    private val commoditiesFromUI = mutableListOf<CommodityUI>()
+    private val commoditiesFromUI = mutableListOf<CommodityUI>()  // Курчавов Алексей 204 группа СПБКИТ
     var totalSumTextView: TextView? = null
-    var currentNotifier: Notifier? = null
+    var currentNotifier: Notifier? = null  // Курчавов Алексей 204 группа СПБКИТ
 
     fun addUICommodity(checkBoxIncluded: CheckBox, textViewCount: EditText, textViewPrice: EditText, textViewTotalPrice: TextView) {
-        commoditiesFromUI.add(
+        commoditiesFromUI.add(  // Курчавов Алексей 204 группа СПБКИТ
             CommodityUI(checkBoxIncluded, textViewCount, textViewPrice, textViewTotalPrice)
         )
-    }
+    }  // Курчавов Алексей 204 группа СПБКИТ
 
-    fun calculate() {
+    fun calculate() {  // Курчавов Алексей 204 группа СПБКИТ
         var sum = 0f
         try {
             for (commodity in commoditiesFromUI) {
                 val commoditiesPrice = commodity.getTotalPrice()
-                sum += commoditiesPrice
+                sum += commoditiesPrice  // Курчавов Алексей 204 группа СПБКИТ
                 commodity.setCommodityTotal()
             }
             setTotalSum(sum.toString())
 
         } catch (e: NoValueException) {
-            setTotalSum(e.message ?: "Error")
+            setTotalSum(e.message ?: "Error")  // Курчавов Алексей 204 группа СПБКИТ
         }
     }
 
     private fun setTotalSum(msg: String) {
         totalSumTextView?.text = msg
-        currentNotifier?.notify(msg)
+        currentNotifier?.notify(msg)  // Курчавов Алексей 204 группа СПБКИТ
     }
 }
 
 data class CommodityUI(
-    val checkBoxIncluded: CheckBox,
+    val checkBoxIncluded: CheckBox,  // Курчавов Алексей 204 группа СПБКИТ
     val textViewCount: EditText,
     val textViewPrice: EditText,
     val textViewTotalPrice: TextView
     ) {
 
     private val name = checkBoxIncluded.text.toString()
-    private val count: Int
+    private val count: Int  // Курчавов Алексей 204 группа СПБКИТ
         get() = run {
             return try {
                 textViewCount.text.toString().toInt()
             } catch (e: NumberFormatException) {
                 throw NoValueException("No value in number $name field")
-            }
+            }  // Курчавов Алексей 204 группа СПБКИТ
         }
 
     private val price: Float
         get() = run {
             return try {
-                textViewPrice.text.toString().toFloat()
+                textViewPrice.text.toString().toFloat()  // Курчавов Алексей 204 группа СПБКИТ
             } catch (e: NumberFormatException) {
                 throw NoValueException("No value in price $name field")
             }
         }
-
+  // Курчавов Алексей 204 группа СПБКИТ
     fun getTotalPrice(): Float {
         return if (checkBoxIncluded.isChecked) {
             count * price
         } else {
             0f
-        }
+        }  // Курчавов Алексей 204 группа СПБКИТ
     }
 
     fun setCommodityTotal() {
-        textViewTotalPrice.text = getTotalPrice().toString()
+        textViewTotalPrice.text = getTotalPrice().toString()  // Курчавов Алексей 204 группа СПБКИТ
     }
 }
 
-class NoValueException(message: String) : Exception(message)
+class NoValueException(message: String) : Exception(message)  // Курчавов Алексей 204 группа СПБКИТ
 
 
 abstract class Notifier {
-    abstract fun notify(msg: String): Unit
+    abstract fun notify(msg: String): Unit  // Курчавов Алексей 204 группа СПБКИТ
 }
 
 class ToastNotifier(private val ctx: Context) : Notifier() {
 
-    override fun notify(msg: String) {
+    override fun notify(msg: String) {  // Курчавов Алексей 204 группа СПБКИТ
         Toast.makeText(ctx, msg, Toast.LENGTH_SHORT).show()
     }
 }
 
 class WindowBoxNotifier(private val ctx: Context): Notifier() {
     override fun notify(msg: String) {
-        val builder = AlertDialog.Builder(ctx)
+        val builder = AlertDialog.Builder(ctx)  // Курчавов Алексей 204 группа СПБКИТ
         builder.create()
         builder.setIcon(R.drawable.dialog_img)
         builder.setTitle(ctx.getString(R.string.total_commodity))
-        builder.setMessage(msg)
+        builder.setMessage(msg)  // Курчавов Алексей 204 группа СПБКИТ
         builder.show()
     }
 }
--- app/src/main/java/com/spbkit/kurchavov204/lab04/MainActivity.kt
+++ app/src/main/java/com/spbkit/kurchavov204/lab04/MainActivity.kt
@@ -8,57 +8,57 @@ import android.widget.RadioGroup
 
 
 class MainActivity : AppCompatActivity() {
-    private val goods = CommoditiesUI()
+    private val goods = CommoditiesUI()  // Курчавов Алексей 204 группа СПБКИТ
     private val toaster = ToastNotifier(this)
     private val dialoger = WindowBoxNotifier(this)
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {  // Курчавов Алексей 204 группа СПБКИТ
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         goods.totalSumTextView = findViewById(R.id.textViewTotalSum)
-        goods.currentNotifier = toaster
+        goods.currentNotifier = toaster  // Курчавов Алексей 204 группа СПБКИТ
 
         goods.addUICommodity(
             findViewById(R.id.checkBoxApple),
             findViewById(R.id.editTextNumberApple),
-            findViewById(R.id.editTextPriceApple),
+            findViewById(R.id.editTextPriceApple),  // Курчавов Алексей 204 группа СПБКИТ
             findViewById(R.id.textViewTotalApple)
         )
 
         goods.addUICommodity(
-            findViewById(R.id.checkBoxStrawberry),
+            findViewById(R.id.checkBoxStrawberry),  // Курчавов Алексей 204 группа СПБКИТ
             findViewById(R.id.editTextNumberStrawberry),
             findViewById(R.id.editTextPriceStrawberry),
             findViewById(R.id.textViewTotalStrawberry)
         )
-
+          // Курчавов Алексей 204 группа СПБКИТ
         goods.addUICommodity(
             findViewById(R.id.checkBoxBlueberry),
             findViewById(R.id.editTextNumberBlueberry),
-            findViewById(R.id.editTextPriceBlueberry),
+            findViewById(R.id.editTextPriceBlueberry),  // Курчавов Алексей 204 группа СПБКИТ
             findViewById(R.id.textViewTotalBlueberry)
         )
 
         goods.addUICommodity(
-            findViewById(R.id.checkBoxPotatoes),
+            findViewById(R.id.checkBoxPotatoes),  // Курчавов Алексей 204 группа СПБКИТ
             findViewById(R.id.editTextNumberPotatoes),
             findViewById(R.id.editTextPricePotatoes),
             findViewById(R.id.textViewTotalPotatoes)
-        )
+        )  // Курчавов Алексей 204 группа СПБКИТ
 
         findViewById<Button>(R.id.buttonCalculate).setOnClickListener(View.OnClickListener {
             goods.calculate()
-        })
+        })  // Курчавов Алексей 204 группа СПБКИТ
 
         findViewById<RadioGroup>(R.id.radioGroupNotification).setOnCheckedChangeListener { radioGroup, buttonID ->
             when(buttonID) {
                 R.id.radioButtonToast -> goods.currentNotifier = toaster
                 R.id.radioButtonDialogWindow -> goods.currentNotifier = dialoger
             }
-        }
+        }  // Курчавов Алексей 204 группа СПБКИТ
     }
 
 
-}
+}  // Курчавов Алексей 204 группа СПБКИТ
